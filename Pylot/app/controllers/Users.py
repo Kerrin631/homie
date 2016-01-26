94d283e6406c3662273d94ac840f61496fdb37f8
Kerrin631 <kerrin.arora@gmail.com>
2016-01-26 11:46:30 -0800

    first commit

---
+++ Pylot/app/controllers/Users.py
@@ -0,0 +1,67 @@
+from system.core.controller import *
+
+class Users(Controller):
+    def __init__(self, action):
+        super(Users, self).__init__(action)
+        self.load_model('User')
+
+    def index(self):
+
+
+        return self.load_view('index.html')
+
+    def register(self):
+        user_info = {
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email'],
+        'password' : request.form['password'],
+        'confirm_password' : request.form['confirm_password']
+        }
+
+        create_status = self.models['User'].register_user(user_info)
+
+        if create_status['status'] == True:
+            session['id'] = create_status['user']['id']
+            return redirect('/success')
+        else:
+            for message in create_status['errors']:
+                flash(message, 'regis_errors')
+            return redirect('/')
+
+    def login(self):
+        user_info = {
+        'email' : request.form['email'],
+        'password' : request.form['password']
+        }
+
+        status = self.models['User'].login_user(user_info)
+
+        if status['status'] == False:
+            for message in status['errors']:
+                flash(message, 'Login_errors')
+            return redirect('/')
+        else:
+            session['name'] = status['user']['first_name']
+            return redirect('/home')
+
+    def success(self):
+        return self.load_view('success.html')
+
+    def home(self):
+        return self.load_view('home.html')
+
+    def profile(self):
+        return self.load_view('profile.html')
+
+    def friends_page(self):
+        return redirect('/home')
+
+    def communicate(self):
+        return self.load_view('messages.html')
+
+    def messages_page(self):
+        return redirect('/communicate')
+
+    def register_page(self):
+        return self.load_view('register.html')
---
+++ Pylot/app/models/User.py
@@ -0,0 +1,70 @@
+from system.core.model import Model
+
+import re
+class User(Model):
+    def __init__(self):
+        super(User, self).__init__()
+
+    def register_user(self,user_info):
+        EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+        errors = []
+
+        if not user_info['first_name']:
+            errors.append('First Name cannot be empty')
+
+        if len(user_info['first_name']) < 3:
+            errors.append('First Name must be longer than 3 character')
+        if not user_info['last_name']:
+            errors.append('Name cannot be empty')
+        if len(user_info['last_name']) < 3:
+            errors.append('Last Name must be longer than 3 character')
+        if not user_info['email']:
+            errors.append('Email field cannot be empty')
+        if not EMAIL_REGEX.match(user_info['email']):
+            errors.append('Email not a valid format')
+        if not user_info['password']:
+            errors.append('Password field cannot be empty')
+        if len(user_info['password']) < 5:
+            errors.append('Password must be longer than 4 characters')
+        if not user_info['confirm_password']:
+            errors.append('Confirm Password field cannot be empty')
+        if user_info['password'] != user_info['confirm_password']:
+            errors.append('Passwords do not match')
+            print errors
+        if errors:
+            return {'status': False, 'errors' : errors}
+        else:
+            hashed_pw = self.bcrypt.generate_password_hash(user_info['password'])
+            query = "INSERT INTO users (first_name,last_name,email,password) VALUES (%s,%s,%s,%s)"
+            data = [user_info['first_name'],user_info['last_name'],user_info['email'],hashed_pw]
+
+            self.db.query_db(query,data)
+
+            get_user = "Select * From users Order By id DESC LIMIT 1"
+            user = self.db.query_db(get_user)
+            return {'status' : True , 'user' : user[0]}
+
+    def login_user(self,user_info):
+        errors = []
+        if not user_info['password']:
+            errors.append('Password Cannot be empty')
+
+        if not user_info['email']:
+            errors.append('Email Cannot be empty')
+
+        if errors:
+            return {'status' : False, 'errors': errors}
+        else:
+            query = 'Select * From users Where email = %s'
+            data = [user_info['email']]
+            user = self.db.query_db(query,data)
+
+            if len(user) > 0:
+                if self.bcrypt.check_password_hash(user[0]['password'], user_info['password']):
+                    return {'status' : True, 'user' : user[0]}
+                else:
+                    errors.append('Password or Email is Invalid')
+                    return {'status' : False, 'errors': errors}
+            else:
+                errors.append('Email was not found')
+                return {'status' : False, 'errors': errors}
