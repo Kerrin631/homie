5df81ac0ebca2404e6d559708160924efaacfa8b
fordwright21 <fordwright@gmail.com>
2016-01-29 11:08:47 -0800

    Test Commit

--- Pylot/app/controllers/Matches.py
+++ Pylot/app/controllers/Matches.py
@@ -8,9 +8,12 @@ class Matches(Controller):
         self.load_model('User')
 
     def process_home(self):
-        current_user = self.models['Match'].get_potential_user(session['id'])
-        print current_user['id']
-        destination = '/home/' + str(current_user['id'])
+        try:
+            current_user = self.models['Match'].get_potential_user(session['id'])
+            destination = '/home/' + str(current_user['id'])
+        except ValueError:
+            destination = '/home/' + "k"
+        
         return redirect (destination)
 
 
@@ -20,15 +23,19 @@ class Matches(Controller):
         #select statement that picks a random id
         #if that match value == null && location is within range
         #then bring up the new picture
-        current_user = self.models['Match'].get_user_by_id(friend_id)
+        try:
+            current_user = self.models['Match'].get_user_by_id(friend_id)
+            friend_location = str(current_user['latitude']) + ',' + str(current_user['longitude'])
+            user_location = str(user_info['latitude']) + ',' + str(user_info['longitude'])
+            user_info = self.models['User'].get_location_by_id(session['id'])
+            distance = (vincenty(user_location, friend_location).miles)
+            pic = "https://graph.facebook.com/v2.5/" + str(current_user['clientID']) + "/picture?width=350&height=350"
 
-        user_info = self.models['User'].get_location_by_id(session['id'])
-        user_location = str(user_info['latitude']) + ',' + str(user_info['longitude'])
-        friend_location = str(current_user['latitude']) + ',' + str(current_user['longitude'])
-
-        print (vincenty(user_location, friend_location).miles)
-        distance = (vincenty(user_location, friend_location).miles)
-        pic = "https://graph.facebook.com/v2.5/" + str(current_user['clientID']) + "/picture?width=350&height=350"
+        except IndexError:
+            current_user = 0
+            distance = 0
+            pic = 0
+        
         return self.load_view('home.html', current_user=current_user, distance = distance, pic=pic)
 
     def process_match(self,friend_id):
